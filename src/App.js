2f3685650d3d73d242d8047e98dda90f3b82c162
unknown <alexandre912323@gmail.com>
2022-03-18 16:19:16 +0100

    first commit ca marche

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ColorPicker from './colorPicker/ColorPicker'
+
+function App() {
+  return (
+    <div className="App">
+        <ColorPicker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/colorPicker/ColorPicker.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import './ColorPicker.css'
+
+class ColorPicker extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            color1: '#ffffff',
+            color2: '#619d34',
+            color3: '#000000',
+            breakpoint1: '0',
+            breakpoint2: '50',
+            breakpoint3: '100',
+            rotation: '88'
+        }
+    }
+
+    onChange1(event) {
+        this.setState({
+            color1: event.target.value
+        })
+    }
+
+    onChange2(event) {
+        this.setState({
+            color2: event.target.value
+        })
+    }
+
+    onChange3(event) {
+        this.setState({
+            color3: event.target.value
+        })
+    }
+
+    changeBreakpoint1(event) {
+        this.setState({
+            breakpoint1: event.target.value
+        })
+    }
+
+    changeBreakpoint2(event) {
+        this.setState({
+            breakpoint2: event.target.value
+        })
+    }
+
+    changeBreakpoint3(event) {
+        this.setState({
+            breakpoint3: event.target.value
+        })
+    }
+
+    changeRotation(event) {
+        this.setState({
+            rotation: event.target.value
+        })
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <h1>CSS GRADIENT</h1>
+                <div className="box" id="color1">
+                    <span>Color 1</span>
+                    <div>
+                        <input type="color" onChange={this.onChange1.bind(this)}></input>
+                    </div>
+
+                    <div>
+                        <label>Breakpoint</label>
+                        <input type="number" onChange={this.changeBreakpoint1.bind(this)}></input>
+                    </div>
+                </div>
+
+                <div className="box">
+                    <span>Color 2</span>
+                    <div>
+                        <input type="color" onChange={this.onChange2.bind(this)}></input>
+                    </div>
+
+                    <div>
+                        <label>Breakpoint</label>
+                        <input type="number" onChange={this.changeBreakpoint2.bind(this)}></input>
+                    </div>
+                </div> 
+
+                <div className="box">
+                    <span>Color 3</span>
+                    <div>
+                        <input type="color" onChange={this.onChange3.bind(this)}></input>
+                    </div>
+
+                    <div>
+                        <label>Breakpoint</label>
+                        <input type="number" onChange={this.changeBreakpoint3.bind(this)}></input>
+                    </div>
+                </div>
+
+                <div className="box">
+                    <label>Rotation</label>
+                    <input type='range' onChange={this.changeRotation.bind(this)}></input>
+                </div>
+
+                <div className="box2" style={{background: `linear-gradient(${this.state.rotation + 'deg'}, ${this.state.color1} ${this.state.breakpoint1 + '%'}, ${this.state.color2} ${this.state.breakpoint2 + '%'}, ${this.state.color3} ${this.state.breakpoint3 + '%'})`}}>
+                    coucou
+                </div>
+
+            </div>
+            
+        ); 
+    }
+    
+}
+
+export default ColorPicker;
