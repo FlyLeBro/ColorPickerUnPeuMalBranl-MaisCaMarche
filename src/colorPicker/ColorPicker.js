8cfa7c6c227d8bf3f10c6ad027e2cd2496b3f1f5
unknown <alexandre912323@gmail.com>
2022-03-18 16:40:58 +0100

    add default value

--- src/colorPicker/ColorPicker.js
+++ src/colorPicker/ColorPicker.js
@@ -64,42 +64,42 @@ class ColorPicker extends React.Component {
                 <div className="box">
                     <span>Color 1</span>
                     <div>
-                        <input type="color" onChange={this.onChange1.bind(this)}></input>
+                        <input value={this.state.color1} type="color" onChange={this.onChange1.bind(this)}></input>
                     </div>
 
                     <div>
                         <label>Breakpoint</label>
-                        <input type="number" onChange={this.changeBreakpoint1.bind(this)}></input>
+                        <input value={this.state.breakpoint1} type="number" onChange={this.changeBreakpoint1.bind(this)}></input>
                     </div>
                 </div>
 
                 <div className="box">
                     <span>Color 2</span>
                     <div>
-                        <input type="color" onChange={this.onChange2.bind(this)}></input>
+                        <input value={this.state.color2} type="color" onChange={this.onChange2.bind(this)}></input>
                     </div>
 
                     <div>
                         <label>Breakpoint</label>
-                        <input type="number" onChange={this.changeBreakpoint2.bind(this)}></input>
+                        <input value={this.state.breakpoint2} type="number" onChange={this.changeBreakpoint2.bind(this)}></input>
                     </div>
                 </div> 
 
                 <div className="box">
                     <span>Color 3</span>
                     <div>
-                        <input type="color" onChange={this.onChange3.bind(this)}></input>
+                        <input value={this.state.color3} type="color" onChange={this.onChange3.bind(this)}></input>
                     </div>
 
                     <div>
                         <label>Breakpoint</label>
-                        <input type="number" onChange={this.changeBreakpoint3.bind(this)}></input>
+                        <input value={this.state.breakpoint3} type="number" onChange={this.changeBreakpoint3.bind(this)}></input>
                     </div>
                 </div>
 
                 <div className="box">
                     <label>Rotation</label>
-                    <input type='range' onChange={this.changeRotation.bind(this)}></input>
+                    <input value={this.state.rotation} type='range' onChange={this.changeRotation.bind(this)}></input>
                 </div>
 
                 <div className="box2" style={{background: `linear-gradient(${this.state.rotation + 'deg'}, ${this.state.color1} ${this.state.breakpoint1 + '%'}, ${this.state.color2} ${this.state.breakpoint2 + '%'}, ${this.state.color3} ${this.state.breakpoint3 + '%'})`}}>
